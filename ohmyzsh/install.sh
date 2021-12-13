1cf59029e111452a82b877151be1d0f0c6ae190e
Matthew Ferderber <matthew.ferderber@shopify.com>
2021-12-13 16:47:20 -0500

    prevent ohmyzsh from replacing zshrc

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
